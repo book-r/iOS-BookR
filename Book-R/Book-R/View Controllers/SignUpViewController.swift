376b85fd6e7b9da9e65b83ae1cb00a2c0aed63e0
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-20 14:05:02 -0700

    signUpView and homeView

---
+++ Book-R/Book-R/View Controllers/SignUpViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SignUpViewController.swift
+//  Book-R
+//
+//  Created by Hector Steven on 5/20/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
