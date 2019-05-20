a8fa34932d80fee6472a9bb9d768033d1a1d2580
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-20 14:14:55 -0700

    sign in and sign up outlets

--- Book-R/Book-R/View Controllers/SignUpViewController.swift
+++ Book-R/Book-R/View Controllers/SignUpViewController.swift
@@ -16,6 +16,23 @@ class SignUpViewController: UIViewController {
         
     }
     
-
-
+	@IBAction func signUpButton(_ sender: Any) {
+		guard let username = usernameLabel.text,
+			let password = passwordLabel.text,
+			let retypePassword = reTypePasswordLabel.text else { return }
+		
+		
+		print(username," - ", password, "-", retypePassword)
+		
+		usernameLabel.text = nil
+		passwordLabel.text = nil
+		reTypePasswordLabel.text = nil
+		
+		
+	}
+	@IBOutlet var signUpButtonOutlet: UIButton!
+	
+	@IBOutlet var usernameLabel: UITextField!
+	@IBOutlet var passwordLabel: UITextField!
+	@IBOutlet var reTypePasswordLabel: UITextField!
 }
