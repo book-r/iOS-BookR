c65ca66369ccbf10344dde4a64a0e580d9bf5a3a
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-20 12:31:13 -0700

    User model

---
+++ Book-R/Book-R/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Book-R
+//
+//  Created by Hector Steven on 5/20/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+	let username: String
+	let password: String
+}
