87b1cfdb2a0dd71f81bdb58e596681e374d6114c
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-20 10:28:25 -0700

    NetworkController

---
+++ Book-R/Book-R/Controllers/NetworkController.swift
@@ -0,0 +1,20 @@
+//
+//  NetworkController.swift
+//  Book-R
+//
+//  Created by Hector Steven on 5/20/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class NetworkController {
+	
+	//network methods
+	
+	
+	
+	
+	
+	
+}
---
+++ Book-R/Book-R/View Controllers/LogInViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LogInViewController.swift
+//  Book-R
+//
+//  Created by Hector Steven on 5/20/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
